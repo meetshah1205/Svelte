3221bbca8e1a3f2a07b15be2e79728ee6c4d955e
Meet Shah <meetshah9625@gmail.com>
2025-04-10 15:18:18 +0530

    Initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	let name = $state('Meet');
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+	let full_name = $derived(name + ' ' + 'Shah');
+
+	// function toggle() {
+	//     status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	// }
+</script>
+
+<Header {name} fake_name="Envixy"/>
+<h2>{full_name}</h2>
+
+<input type="text" bind:value={name} />
+<p>The store is now {status}</p>
+<button
+	onclick={() => {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}}>Toggle status</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	// Used lang="ts" bcuz why not
+	let {
+		name,
+		fake_name = null // Put null as value if not using TS
+	}: {
+		name: string,
+		fake_name?: string | null, // Optional prop
+	} = $props(); // Whatever variable we create in {} will have values passed from the usage
+</script>
+
+<h2>{name.replaceAll('o', 'x')}</h2>	
+{fake_name}
+<style>
+</style>
