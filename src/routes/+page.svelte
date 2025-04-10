2c68742caa050b1775e0b75e26d52330c14fcec7
Meet Shah <meetshah9625@gmail.com>
2025-04-10 15:35:49 +0530

    Template conditionals and Logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,49 @@
 <script lang="ts">
 	import Header from './Header.svelte';
-	let name = $state('Meet');
-	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
-	let full_name = $derived(name + ' ' + 'Shah');
 
-	// function toggle() {
-	//     status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
-	// }
+	let formState = $state({
+		name: '',
+		birthday: '',
+		step: 0,
+		error: ''
+	});
 </script>
 
-<Header {name} fake_name="Envixy"/>
-<h2>{full_name}</h2>
+<main>
+	<p>Step: {formState.step + 1}</p>
+	{#if formState.error !== ''}
+		<p style="color: red; font-family: sans-serif;">Error: {formState.error}</p>
+	{/if}
 
-<input type="text" bind:value={name} />
-<p>The store is now {status}</p>
-<button
-	onclick={() => {
-		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
-	}}>Toggle status</button>
+	{#if formState.step === 0}
+		<div>
+			<label for="name">Your Name</label>
+			<input type="text" id="name" bind:value={formState.name} />
+		</div>
+		<button
+			onclick={() => {
+				if (formState.name !== '') {
+					formState.step++;
+					formState.error = '';
+				} else {
+					formState.error = 'Ur name is empty. Provide a name';
+				}
+			}}>Next</button
+		>
+	{:else if formState.step === 1}
+		<div>
+			<label for="birthday">Your Birthday</label>
+			<input type="date" id="bday" bind:value={formState.birthday} />
+		</div>
+		<button
+			onclick={() => {
+				if (formState.birthday !== '') {
+					formState.step++;
+					formState.error = '';
+				} else {
+					formState.error = 'Ur birthday is empty. Provide a birthday';
+				}
+			}}>Next</button
+		>
+	{/if}
+</main>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,12 +4,13 @@
 		name,
 		fake_name = null // Put null as value if not using TS
 	}: {
-		name: string,
-		fake_name?: string | null, // Optional prop
+		name: string;
+		fake_name?: string | null; // Optional prop
 	} = $props(); // Whatever variable we create in {} will have values passed from the usage
 </script>
 
-<h2>{name.replaceAll('o', 'x')}</h2>	
+<h2>{name.replaceAll('o', 'x')}</h2>
 {fake_name}
+
 <style>
 </style>
